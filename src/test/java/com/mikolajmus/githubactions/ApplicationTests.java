5da44903998e1c6bd851f8fc6f4f23713d6176bc
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-12-22 13:04:18 +0100

    Initial commit

---
+++ src/test/java/com/mikolajmus/githubactions/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikolajmus.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
